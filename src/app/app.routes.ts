f787bd1a026bec7e5b84ca36881e34493b058785
MaxiLedesma04 <maxinglede@gmail.com>
2024-10-25 11:36:04 -0300

    Primer Maquetado final

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: MainComponent }, // Ruta raíz para el componente principal
+  { path: '**', redirectTo: '', pathMatch: 'full' }, // Redireccionar cualquier URL no válida a la raíz
+];
